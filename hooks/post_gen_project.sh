da8be11b4744e8de5112b8f3a9ca62e36f18c44b
Larry Fenn <lfenn@ap.org>
2025-04-30 12:56:18 -0400

    Add uv commands; still need to figure out jupyter stuff

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+## This post project generation script only runs if uv is on the machine
+command -v uv >/dev/null 2>&1 || { echo >&2 "uv not found.  Aborting startup script."; exit 1; }
+
+## Init uv & virtualenv
+uv init
+uv venv
